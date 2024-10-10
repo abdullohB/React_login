003e4870d2bba0e56d85486c724a6cf7eb31ca7f
abdullohB <abdulloh8062@gmail.com>
2024-10-10 09:29:02 +0700

    insert number 1

--- src/Login.js
+++ src/Login.js
@@ -24,7 +24,7 @@ function Login() {
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
-                <h2>Sing in</h2>
+                <h2>Sing in1</h2>
                 
                 <from action="" onSubmit={handleSubmit}>
                     <div className='mb-3'>
